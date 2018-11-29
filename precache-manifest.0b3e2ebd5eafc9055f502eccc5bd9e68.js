09d41e6f4ae082455e71abb6f01a4e21a734a4c6
Michael Corey <fishincorey@gmail.com>
2018-11-28 20:22:19 -0700

    Updates

---
+++ precache-manifest.0b3e2ebd5eafc9055f502eccc5bd9e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "fc2c3e7325b2acc0a806",
+    "url": "/tictactoe/static/js/main.fc2c3e73.chunk.js"
+  },
+  {
+    "revision": "07861e9d5e216d6b0c3e",
+    "url": "/tictactoe/static/js/1.07861e9d.chunk.js"
+  },
+  {
+    "revision": "fc2c3e7325b2acc0a806",
+    "url": "/tictactoe/static/css/main.74db0638.chunk.css"
+  },
+  {
+    "revision": "9f320d2b74dc575b017d940ede41aad5",
+    "url": "/tictactoe/index.html"
+  }
+];
